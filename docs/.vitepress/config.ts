f81a4e17bb7247b5edcdde35a13665515194df7a
ascrevia <crevia12@yahoo.com>
2026-05-17 13:38:40 +0300

    fix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Group Mini Docs',
   base: '/',
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }]
+  ],
 
   locales: {
     root: {
